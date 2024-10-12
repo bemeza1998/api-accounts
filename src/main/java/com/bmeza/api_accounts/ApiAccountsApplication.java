c4d1bde2258b3635fce03aa6d5f82d5f218bac7f
bemeza <esteban-meza85@hotmail.com>
2024-10-11 23:46:11 -0500

    first commit

---
+++ src/main/java/com/bmeza/api_accounts/ApiAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.bmeza.api_accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmeza/api_accounts/ApiAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmeza.api_accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
